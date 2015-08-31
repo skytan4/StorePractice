9fd9a66c17cb5407aa14ae30883132824b9d98d5
Skyler Tanner <tannerskyler@gmail.com>
2015-08-31 09:23:52 -0600

    initial commit

---
+++ StorePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StorePractice
+//
+//  Created by Skyler Tanner on 8/31/15.
+//  Copyright (c) 2015 Skyler Tanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StorePractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StorePractice
+//
+//  Created by Skyler Tanner on 8/31/15.
+//  Copyright (c) 2015 Skyler Tanner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
